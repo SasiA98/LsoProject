71779c3165d6d24f728de4fbd27d4e6266bac25e
TheDoctorGenny <59081805+TheDoctorGenny@users.noreply.github.com>
2019-12-20 18:20:49 +0100

    Add files via upload

---
+++ client/client_library.h
@@ -0,0 +1,33 @@
+#ifndef CLIENT_LIBRARY_H
+#define CLIENT_LIBRARY_H
+
+#include <unistd.h>
+#include <netdb.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <string.h>
+#include <assert.h>
+#include <signal.h>
+#include <sys/socket.h> 
+#include <arpa/inet.h>
+#include <stdbool.h>
+
+#define PORT 8080
+#define IP_PORT "127.0.0.1"
+
+typedef unsigned int uint;
+
+int getInt();
+void clientFunctions(int sockfd);
+int dimension(int socketfd);
+void getStr(char * str, uint len);
+
+typedef struct parameters {
+	int choice;
+    int from;
+    int to;
+    int dimFile;
+    char *buffer[];
+}parameters;
+
+#endif
