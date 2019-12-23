68eda58f3050806b10f7d1fbb509d7b3730982e3
TheDoctorGenny <59081805+TheDoctorGenny@users.noreply.github.com>
2019-12-23 16:42:14 +0100

    Add files via upload

---
+++ server/server_library.c
@@ -0,0 +1,133 @@
+#include "server_library.h"
+
+
+int CreateSocket(){
+
+    int socketfd;
+    struct sockaddr_in serveraddr;
+
+    socketfd=socket(AF_INET,SOCK_STREAM,0);
+    if (socketfd == -1) { 
+		perror("Socket creation failed"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully created..\n"); 
+
+    serveraddr.sin_family=AF_INET;
+    serveraddr.sin_addr.s_addr=INADDR_ANY;
+    serveraddr.sin_port=htons(PORT);
+
+    //Bind del socket
+    if ((bind(socketfd, (struct sockaddr*)&serveraddr, sizeof(serveraddr))) != 0) { 
+		perror("Socket bind failed"); 
+		exit(0); 
+	} 
+	else
+		printf("Socket successfully binded..\n");   
+
+    // Now server is ready to listen and verification 
+	if ((listen(socketfd, 5)) != 0) { 
+		perror("Listen failed"); 
+		exit(0); 
+	} 
+	else
+		printf("Server listening..\n"); 
+
+    return socketfd;
+}
+
+void* mainThread(void* arg){
+
+    bool flag = true;
+
+    parameters *par = (parameters *)malloc(sizeof(parameters));
+	management *man = (management *)malloc(sizeof(management));
+
+    pthread_t threadDim, threadRead, threadWrite;
+	int connectfd = *((int *)arg);
+	
+	man->connectfd = connectfd;
+
+	while(flag){
+		
+		unsigned char buffer[1000];
+        if(0 == read(connectfd, &buffer, sizeof(buffer)))
+		   flag = false;
+		else{
+		   par = deserializeParameters(buffer, par);
+	       man->par = par;
+
+        	if(par->choice==1){
+	    		pthread_create(&threadDim,NULL,dimThread,man);
+		    	pthread_join(threadDim,NULL);
+ 	 	    }  
+     	    if(par->choice==2){
+            	pthread_create(&threadRead,NULL,readThread,man);
+		     	pthread_join(threadRead,NULL);
+		    } 
+		    if(par->choice==3){
+             	pthread_create(&threadWrite,NULL,writeThread,man);
+		    	pthread_join(threadWrite,NULL);
+	    	}
+		}
+	}
+	free(man);
+	free(arg);
+	free(par);
+	
+	return NULL;
+}
+
+void* readThread(void* arg){   //I thinked to make an infinit cicle to wait the correct input of the client, but if the client dies, the server remains in loop and the management of mutex is more difficult : sasi
+	management *man = ((management *) arg);
+
+	int fd, offset;
+	char bufferFile[1000];
+	unsigned char buffer[1000];
+
+	int bufferSize = man->par->to - man->par->from; // when read from keyboard check that from is smaller then to : sasy 
+
+    if ((fd = open("Document.txt", O_RDONLY)) == -1) //Does it need any permission? : sasy    
+	    perror("open error");   
+  
+    if ((offset = lseek(fd, (off_t) 0, SEEK_END)) == -1)
+	     perror("lseek error");      
+	
+	if(offset < man->par->to){ 		 //We could shift lseek directly in 'to' position and check if it gives a error.    
+	   strcpy(man->par->buffer,"ERRORE: l'offset è troppo grande\0");
+	   man->par->error = true;
+
+	}else{
+		if (lseek(fd, (off_t) man->par->from, SEEK_SET) == -1)       
+	    perror("lseek error");   
+	
+		if (bufferSize != read(fd, bufferFile, bufferSize))    
+	        perror("read error"); 
+
+	    bufferFile[bufferSize]='\0';	 
+		strcpy(man->par->buffer,bufferFile); 
+	}
+    serializeParameters(buffer, man->par); 
+    write(man->connectfd, &buffer, sizeof(buffer)); 
+    
+	close(fd);
+    return NULL;
+}
+   
+void* writeThread(void* arg){ 
+    return NULL;
+} 
+
+void* dimThread(void* arg){ 
+    
+	management *man = ((management *) arg);
+    unsigned char buffer[1000]; 
+
+    strcpy(man->par->buffer,"EOO");
+	man->par->dimFile=123;
+	serializeParameters(buffer, man->par); 
+  	write(man->connectfd, &buffer, sizeof(buffer));  
+
+    return NULL;
+} 
---
+++ server/server_library.h
@@ -0,0 +1,39 @@
+#ifndef SERVER_LIBRARY_H
+#define SERVER_LIBRARY_H
+
+#include <unistd.h>
+#include <stdio.h> 
+#include <netdb.h> 
+#include <pthread.h>
+#include <netinet/in.h> 
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/socket.h> 
+#include <sys/types.h>
+#include <fcntl.h>
+#include <sys/stat.h> 
+#include "../protocol/protocol.h"
+
+#define PORT 8080
+
+
+struct mutex {
+
+} mutex;
+
+
+typedef struct management{
+
+    int connectfd;
+    parameters *par;
+}management;
+
+
+int CreateSocket();
+void* mainThread(void* arg);
+void* readThread(void* arg);
+void* writeThread(void* arg);
+void* dimThread(void* arg);
+
+
+#endif
