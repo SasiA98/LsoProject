c3221338b689bd134d33401e9743825cd5efe9b7
TheDoctorGenny <59081805+TheDoctorGenny@users.noreply.github.com>
2019-12-23 16:39:49 +0100

    Update client_library.c

--- client/client_library.c
+++ client/client_library.c
@@ -1,14 +1,6 @@
 #include "client_library.h"
 
 
-unsigned char * serialize_int(unsigned char *buffer, int value);
-unsigned char * serialize_char(unsigned char *buffer, char value);
-int deserialize_int(unsigned char *buffer);
-char deserialize_char(unsigned char *buffer);
-void serializeParameters(unsigned char* msg, parameters *par);
-parameters *deserializeParameters(unsigned char* buffer, parameters *par);
-
-
 void getStr(char * str, uint len)
 {
   assert(str != NULL);
@@ -76,6 +68,8 @@ void dimension(int socketfd, parameters *par){
 
 	read(socketfd, &bufferR, sizeof(bufferR));
 	par = deserializeParameters(bufferR, par);
+
+    printf("Dimensione:%d\n",par->dimFile);
 }
 
 void readFile(int socketfd,parameters *par){
@@ -115,87 +109,4 @@ void readFile(int socketfd,parameters *par){
 
 void writeFile(int socketfd, parameters *par){
     printf("Qui si scrive sul file.\n");
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-unsigned char * serialize_int(unsigned char *buffer, int value)
-{
-    buffer[0] = value >> 24;
-    buffer[1] = value >> 16;
-    buffer[2] = value >> 8;
-    buffer[3] = value;
-    return buffer + 4;
-}
-
-unsigned char * serialize_char(unsigned char *buffer, char value)
-{
-    buffer[0] = value;
-    return buffer + 1;
-}
-
-int deserialize_int(unsigned char *buffer)
-{
-    int value = 0;
-
-    value |= buffer[0] << 24;
-    value |= buffer[1] << 16;
-    value |= buffer[2] << 8;
-    value |= buffer[3];
-    return value;
-
-}
-
-char deserialize_char(unsigned char *buffer)
-{
-    return buffer[0];
-}
-
-
-
-void serializeParameters(unsigned char* buffer, parameters *par)
-{
-	buffer = serialize_int(buffer,par->error);
-    buffer = serialize_int(buffer,par->choice);
-    buffer = serialize_int(buffer,par->from);   
-	buffer = serialize_int(buffer,par->to);
-    buffer = serialize_int(buffer,par->dimFile);
-
-	for(int i=0; i<strlen(par->buffer)+1; i++)
-        buffer = serialize_char(buffer,par->buffer[i]);  
-}
-
-
-
-parameters * deserializeParameters(unsigned char* buffer, parameters *par)
-{
-    par->error = deserialize_int(buffer);
-    par->choice = deserialize_int(buffer+4);
-    par->from = deserialize_int(buffer+8);
-    par->to = deserialize_int(buffer+12);
-    par->dimFile = deserialize_int(buffer+16);    
-  
-    int i=0;
-    int j=20;
-    
-    do
-    {
-        par->buffer[i] = deserialize_char((buffer+j));
-        i++;
-        j++;
-    }while(par->buffer[i-1]!='\0'); 
-    
-    j++; 
-    
-    return par;
 }
