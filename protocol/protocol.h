9d93a75fc342adcdc3cd5b203dcc8b5ab7e4e092
TheDoctorGenny <59081805+TheDoctorGenny@users.noreply.github.com>
2019-12-23 16:40:05 +0100

    Create protocol.h

---
+++ protocol/protocol.h
@@ -0,0 +1,25 @@
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
+
+
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct parameters{
+    bool error;	
+	int choice;
+    int from;
+    int to;
+    int dimFile;
+    char buffer[100];
+}parameters;
+
+unsigned char * serialize_int(unsigned char *buffer, int value);
+unsigned char * serialize_char(unsigned char *buffer, char value);
+int deserialize_int(unsigned char *buffer);
+char deserialize_char(unsigned char *buffer);
+void serializeParameters(unsigned char* msg, parameters *par);
+parameters *deserializeParameters(unsigned char* buffer, parameters *par);
+
+
+#endif
